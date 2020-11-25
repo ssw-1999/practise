d6d884097a3ed0a3ab67c8941b502ea6989137ce
sunshuwei@kuaishou.com <sunshuwei@sswmac.local>
2020-11-25 16:09:13 +0800

    change1

---
+++ practise/component/my-task/src/components/inquiry/creation-time.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label>创建时间</label>
+    <input type="text">
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'creationTime',
+  data () {
+    return {
+      startTime:null,
+      endTime:null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ practise/component/my-task/src/components/inquiry/topic-state.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label>话题状态</label>
+    <select name="topic-state" id="">
+        <option value=""></option>
+        <option value=""></option>
+    </select>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'topicState',
+  data () {
+    return {
+      topicState:null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
